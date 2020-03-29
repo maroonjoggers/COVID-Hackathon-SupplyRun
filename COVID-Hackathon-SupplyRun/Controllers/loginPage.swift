67b6a7943f16913dbd4278475928c87627bdb07d
Vignesh Krishnan <vkrishnan998@gmail.com>
2020-03-28 20:06:19 -0500

    Added all AWS auth UI

---
+++ COVID-Hackathon-SupplyRun/Controllers/loginPage.swift
@@ -0,0 +1,9 @@
+//
+//  loginPage.swift
+//  COVID-Hackathon-SupplyRun
+//
+//  Created by Vignesh Krishnan on 3/28/20.
+//  Copyright © 2020 Vignesh Krishnan. All rights reserved.
+//
+
+import Foundation
